4f9881cd143b33378bb364b7ecdfe32a6b4e5c57
Nishil Patel <nishilpatel112@gmail.com>
2020-11-26 22:32:03 +0530

    qrcode error try 3

--- config/barcode.php
+++ config/barcode.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    'store_path' => public_path("/../../../qrcodeStorage/"),
+    // 'store_path' => public_path("/../../../qrcodeStorage/"),
+    'store_path' => "/home/u697324239/public_html/vwerp/public/qrcodeStorage",
 ];
