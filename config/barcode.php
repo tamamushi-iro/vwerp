676c718e44ff8c82267e03decaca231ed0805a6c
Nishil Patel <nishilpatel112@gmail.com>
2020-11-26 22:45:40 +0530

    qrcodeStore Error Solved.

--- config/barcode.php
+++ config/barcode.php
@@ -1,6 +1,5 @@
 <?php
 
 return [
-    // 'store_path' => public_path("/../../../qrcodeStorage/"),
-    'store_path' => "/home/u697324239/public_html/vwerp/public/qrcodeStorage",
+    'store_path' => (config('app.env') === 'local') ? public_path("/qrcodeStorage/") : "/home/u697324239/public_html/vwerp/public/qrcodeStorage",
 ];
